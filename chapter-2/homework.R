16ac1ce2b846289a9c2d257f247f6b9eb39c1d16
Lily Tomkovic <latomkovic@ucdavis.edu>
2018-11-14 09:48:27 -0800

    Added ---- dividers per problem

--- chapter-2/homework.R
+++ chapter-2/homework.R
@@ -1,6 +1,7 @@
 # homework
 
-## 2M1 Recall the globe tossing model from the chapter. Compute and plot the grid approximate
+## 2M1 ----
+# Recall the globe tossing model from the chapter. Compute and plot the grid approximate
 # posterior distribution for each of the following sets of observations. In each case, assume a uniform
 # prior for p.
 # (1) W, W, W
@@ -40,7 +41,8 @@ n <- 7
 posterior <- compute_posterior(w = w, n = n, prior = prior)
 plot_posterior(x = p_grid, y = posterior)
 
-## 2M2 Now assume a prior for p that is equal to zero when p < 0:5 and is a positive constant when
+## 2M2 ----
+# Now assume a prior for p that is equal to zero when p < 0:5 and is a positive constant when
 # p >= 0:5. Again compute and plot the grid approximate posterior distribution for each of the sets of
 # observations in the problem just above.
 
@@ -64,7 +66,8 @@ n <- 7
 posterior <- compute_posterior(w = w, n = n, prior = prior)
 plot_posterior(x = p_grid, y = posterior)
 
-## 2M3 Suppose there are two globes, one for Earth and one for Mars. The Earth globe is 70% covered
+## 2M3 ----
+# Suppose there are two globes, one for Earth and one for Mars. The Earth globe is 70% covered
 # in water. The Mars globe is 100% land. Further suppose that one of these globes—you don’t know
 # which—was tossed in the air and produced a “land” observation. Assume that each globe was equally
 # likely to be tossed. Show that the posterior probability that the globe was the Earth, conditional on
@@ -76,7 +79,8 @@ unstandardized.posterior <- prior * likelihood
 posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 round( posterior[1], 2) == .23
 
-## 2M4 Suppose you have a deck with only three cards. Each card has two sides, and each side is either
+## 2M4 ----
+# Suppose you have a deck with only three cards. Each card has two sides, and each side is either
 # black or white. One card has two black sides. The second card has one black and one white side. The
 # third card has two white sides. Now suppose all three cards are placed in a bag and shuffled. Someone
 # reaches into the bag and pulls out a card and places it flat on a table. A black side is shown facing up,
@@ -96,7 +100,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # the probability the other size is black is equal to the probability that we've drawn card 1
 posterior[1] == 2/3
 
-## 2M5 Now suppose there are four cards: B/B, B/W, W/W, and another B/B. Again suppose a card is
+## 2M5 ----
+# Now suppose there are four cards: B/B, B/W, W/W, and another B/B. Again suppose a card is
 # drawn from the bag and a black side appears face up. Again calculate the probability that the other
 # side is black.
 
@@ -112,7 +117,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # the probability the other size is black is equal to the probability that we've drawn card 1 or 4
 posterior[1] + posterior[4]
 
-## 2M6 Imagine that black ink is heavy, and so cards with black sides are heavier than cards with white
+## 2M6 ----
+# Imagine that black ink is heavy, and so cards with black sides are heavier than cards with white
 # sides. As a result, it’s less likely that a card with black sides is pulled from the bag. So again assume
 # there are three cards: B/B, B/W, and W/W. After experimenting a number of times, you conclude that
 # for every way to pull the B/B card from the bag, there are 2 ways to pull the B/W card and 3 ways to
@@ -130,7 +136,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # the probability the other size is black is equal to the probability that we've drawn card 1
 posterior[1] == .5
 
-## 2M7 Assume again the original card problem, with a single card showing a black side face up. Before
+## 2M7 ----
+# Assume again the original card problem, with a single card showing a black side face up. Before
 # looking at the other side, we draw another card from the bag and lay it face up on the table. The face
 # that is shown on the new card is white. Show that the probability that the first card, the one showing
 # a black side, has black on its other side is now 0.75. Use the counting method, if you can. Hint: Treat
@@ -153,7 +160,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # which equals the probability that the sequence we've chosen is either (1, 2), or (1, 3)
 posterior[1] + posterior[3] == .75
 
-## 2H1 Suppose there are two species of panda bear. Both are equally common in the wild and live
+## 2H1 ----
+# Suppose there are two species of panda bear. Both are equally common in the wild and live
 #  the same places. They look exactly alike and eat the same food, and there is yet no genetic assay
 # capable of telling them apart. They differ however in their family sizes. Species A gives birth to twins
 # 10% of the time, otherwise birthing a single infant. Species B births twins 20% of the time, otherwise
@@ -174,7 +182,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # probability next birth is set of twins
 sum(likelihood*posterior)
 
-## 2H2 Recall all the facts from the problem above. Now compute the probability that the panda we
+## 2H2 ----
+# Recall all the facts from the problem above. Now compute the probability that the panda we
 # have is from species A, assuming we have observed only the first birth and that it was twins.
 species.1.likelihood <- .1
 species.2.likelihood <- .2
@@ -186,7 +195,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # probability pandas is from species 1
 posterior[1]
 
-## 2H3 Continuing on from the previous problem, suppose the same panda mother has a second birth
+## 2H3 ----
+# Continuing on from the previous problem, suppose the same panda mother has a second birth
 # that it is not twins, but a singleton infant. Compute the posterior probability that this panda is
 # species A.
 
@@ -200,7 +210,8 @@ posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 # probability pandas is from species 1
 posterior[1]
 
-## 2H4 A common boast of Bayesian statisticians is that Bayesian inference makes it easy to use all of
+## 2H4 ----
+# common boast of Bayesian statisticians is that Bayesian inference makes it easy to use all of
 # the data, even if the data are of different types.
 # So suppose now that a veterinarian comes along who has a new genetic test that she claims can
 # identify the species of our mother panda. But the test, like all tests, is imperfect. This is the information
